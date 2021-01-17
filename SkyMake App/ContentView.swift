aa8094a870b06c4bcc7d0a44549ab0032e0da30b
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2021-01-17 19:46:54 +0300

    Push AR App Template

---
+++ SkyMake App/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  SkyMake App
+//
+//  Created by Yigit Kerem Oktay on 17.01.2021.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
